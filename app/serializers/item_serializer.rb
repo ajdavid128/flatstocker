57f685feec9bf455e5fff88210ac93a625ddf6f3
Aaron David <ajdavid128@gmail.com>
2023-01-17 14:16:09 -0600

    found capitalize models in item_serializer that were throwing errors for Items#Index route, changed them and the fetch works

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :item_name, :image_url, :current_stock, :minimum_stock, :notes, :category, :item_url, :brand, :color, :unit_type, :unit_amount
 
-  has_one :Retailer
-  has_one :User
+  has_one :retailer
+  has_one :user
 end
