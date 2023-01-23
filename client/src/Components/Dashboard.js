b48aa7210a3af4e85fea6cc1a846e04b28b9bf04
Aaron David <ajdavid128@gmail.com>
2023-01-22 19:57:20 -0600

    played around with trying to import chart.js library, but ran into error and need to update webpack 4 -> 5? according to github bug reports for chart.js

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -1,9 +1,25 @@
-
+import { useState } from "react";
+import { Segment } from "semantic-ui-react";
+import PieChart from "./PieChart";
+import { UserData } from "./Data";
 
 function Dashboard() {
+
+    const [data, setData] = useState({
+        labels: UserData.map((data) => data.year),
+        datasets: [{
+            label: "Users Gained",
+            data: UserData.map((data) => data.userGain)
+        }]
+    })
+    
+
     return (
         <div id="dash">
-            <h1>DASHBOARD PAGE</h1>
+            <Segment>
+                <PieChart chartData={data}/>
+            </Segment>
+            
         </div>
     )
 };
---
+++ client/src/Components/PieChart.js
@@ -0,0 +1,10 @@
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS } from 'chart.js/auto'
+
+function PieChart({chartData}) {
+    return (
+        <Bar data={chartData} />
+    )
+}
+
+export default PieChart;
